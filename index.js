61cae6e60ba5cc63be4d69094e71bc981118fc4f
Igor Grammatchikov <igorgrammatchikov@Igors-MacBook-Pro.local>
2022-12-17 15:14:52 +0300

    2 step

--- index.js
+++ index.js
@@ -1,11 +1,72 @@
 const telegramApi = require('node-telegram-bot-api');
-// Импорт dotenv для защиты API токена
+const axios = require('axios');
+const qs = require('qs');
 require('dotenv').config()
 
-const bot = new telegramApi(process.env.TOKEN, { polling: true})
 
-bot.on('message', (message) => {
+const getSubjectFromRevision = async () => {
+	const response = await axios.get('https://netology.ru/backend/api/expert/homeworks', {
+		params: {
+			q: {
+				lesson_task_task_type_eq: 1,
+				status_eq: 0
+			},
+		},
+		paramsSerializer: {
+			serialize: (params) => qs.stringify(params, {arrayFormat: 'brackets'})
+		},
+		headers: {
+			Cookie: process.env.COOKIE
+		}
+	});
+	return response.data.homeworks
+		.filter(data => data.reviewer?.id === process.env.USER_ID)
+		.reduce((acc, {task, user, program}) => {
+		return `${acc} \n <b>${task.title}</b>
+		<i>${program.pretty_urlcode}</i>
+		${user.full_name} \n`
+	}, '')
+};
+
+const getNewSubjectForCheck = async () => {
+	const response = await axios.get('https://netology.ru/backend/api/expert/homeworks', {
+		params: {
+			q: {
+				lesson_task_task_type_eq: 1,
+				status_eq: 0
+			},
+		},
+		paramsSerializer: {
+			serialize: (params) => qs.stringify(params, {arrayFormat: 'brackets'})
+		},
+		headers: {
+			Cookie: process.env.COOKIE
+		}
+	});
+	return response.data.homeworks
+		.filter(data => data.lesson_task_experts.map(expert => expert.id === process.env.USER_ID))
+		.reduce((acc, {task, user, program}) => {
+			return `${acc} \n <b>${task.title}</b>
+		<i>${program.pretty_urlcode}</i>
+		${user.full_name} \n`
+		}, '')
+};
+const bot = new telegramApi(process.env.TOKEN, { polling: true});
+// установили боковое меню
+bot.setMyCommands([
+	{ command: "returned_from_revision", description: "Вернулись из доработки" },
+	{ command: "can_check", description: "Можно брать на проверку" },
+]);
+
+bot.on('message', async (message) => {
 	const chatId = message.chat.id;
-	
-	bot.sendMessage(chatId, 'ответ')
+	let template;
+	if (message.text === '/returned_from_revision') {
+		template = await getSubjectFromRevision() || "Нет доработанных заданий";
+	} else if (message.text === '/can_check') {
+		template = await getNewSubjectForCheck() || "Нет возможных заданий на проверку"
+	}
+	bot.sendMessage(chatId, template, {parse_mode : "HTML"})
 })
+
+
